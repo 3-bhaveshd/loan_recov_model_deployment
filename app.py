aee54b9f48d12c654727e0ea4e87ba162d35800f
3-bhaveshd <bhaveshdeshmukh372006@gmail.com>
2026-08-02 16:14:16 +0530

    Reorganize imports and restore app.py functionality

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ with tab1:
 
     with col2:
         st.caption("**Risk & Credit Scores**")
-        grade = st.selectbox("Grade", ["A", "B", "C", "D", "E", "F", "G"], index=1, , help = "A-> low Risk | G->High Risk | Assigned by Lender")
+        grade = st.selectbox("Grade", ["A", "B", "C", "D", "E", "F", "G"], index=1, help = "A-> low Risk | G->High Risk | Assigned by Lender")
         sub_grade = st.text_input("Sub-grade", "B3")
 
         fico_low = st.slider(
